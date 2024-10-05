3d073cc1915aa743a4e390a5d50104c18d70c64f
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-10-05 19:23:07 +0000

    Add shopping bag urls and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # ^ to allow Django to see the MEDIA_URL.
